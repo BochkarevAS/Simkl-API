34491cdae6cd1c713f8c987863f39e858328c07c
BochkarevAS <bochkarev-vl@mail.ru>
2017-12-05 18:23:52 +1000

    Commitic

--- src/AppBundle/Service/SimklParser.php
+++ src/AppBundle/Service/SimklParser.php
@@ -29,31 +29,29 @@ class SimklParser {
 
     public function token($code) {
 
-        $clientId = '9e36b457bc701379ef59849daf7ecf71e02549f551442b962b52a34b105c5d82';
-        $clientSecret = '88fcbdd1433112ac07872b573ad5fed427675ad0d1ee4dde5f8b6580800bc512';
-        $redirectUri = 'http://127.0.0.1:8000/simkl/token';
+        $json = [
+            'code' => $code,
+            'client_id' => '9e36b457bc701379ef59849daf7ecf71e02549f551442b962b52a34b105c5d82',
+            'client_secret' => '88fcbdd1433112ac07872b573ad5fed427675ad0d1ee4dde5f8b6580800bc512',
+            'redirect_uri' => 'http://127.0.0.1:8000/simkl/token',
+            'grant_type' => 'authorization_code'
+        ];
 
-        $ch = curl_init();
+        $json = json_encode($json);
 
-        curl_setopt($ch, CURLOPT_URL, "https://api.simkl.com/oauth/token");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
+        $curl = curl_init();
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-          \"code\": \"$code\",
-          \"client_id\": \"$clientId\",
-          \"client_secret\": \"$clientSecret\",
-          \"redirect_uri\": \"$redirectUri\",
-          \"grant_type\": \"authorization_code\"
-        }");
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        curl_setopt($curl, CURLOPT_URL, "https://api.simkl.com/oauth/token");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
             "Content-Type: application/json"
         ]);
 
-        $response = curl_exec($ch);
-        curl_close($ch);
+        $response = curl_exec($curl);
+        curl_close($curl);
 
         var_dump($response);
 
