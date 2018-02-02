a8ca6e97ba237965348548c026ace0752bd94406
BochkarevAS <bochkarev-vl@mail.ru>
2018-02-02 11:22:23 +1000

    Commitic

--- src/AppBundle/Service/SimklService.php
+++ src/AppBundle/Service/SimklService.php
@@ -3,23 +3,30 @@
 namespace AppBundle\Service;
 
 use AppBundle\Entity\Movie;
+use Doctrine\Common\Cache\Cache;
 use Doctrine\Common\Persistence\ObjectManager;
 use GuzzleHttp\Client;
 
 class SimklService {
 
-    private $manager;
+    const CLIENT_ID = '9e36b457bc701379ef59849daf7ecf71e02549f551442b962b52a34b105c5d82';
+    const REDIRECT_URI = 'http://127.0.0.1:8000/oauth/receive';
+    const CLIENT_SECRET = '88fcbdd1433112ac07872b573ad5fed427675ad0d1ee4dde5f8b6580800bc512';
 
-    public function __construct(ObjectManager $manager) {
+    private $manager;
+    private $cache;
+
+    public function __construct(ObjectManager $manager, Cache $cache) {
         $this->manager = $manager;
+        $this->cache = $cache;
     }
 
     public function redirectToAuthorization() {
         $state = md5(uniqid(mt_rand(), true));
 
         $http = [
-            'client_id'     => '9e36b457bc701379ef59849daf7ecf71e02549f551442b962b52a34b105c5d82',
-            'redirect_uri'  => 'http://127.0.0.1:8000/oauth/receive',
+            'client_id'     => self::CLIENT_ID,
+            'redirect_uri'  => self::REDIRECT_URI,
             'response_type' => 'code',
             'state'         => $state
         ];
@@ -42,9 +49,9 @@ class SimklService {
         $response = $client->request('POST', '/oauth/token', [
             'json' => [
                 'code'          => $code,
-                'client_id'     => '9e36b457bc701379ef59849daf7ecf71e02549f551442b962b52a34b105c5d82',
-                'client_secret' => '88fcbdd1433112ac07872b573ad5fed427675ad0d1ee4dde5f8b6580800bc512',
-                'redirect_uri'  => 'http://127.0.0.1:8000/oauth/receive',
+                'client_id'     => self::CLIENT_ID,
+                'client_secret' => self::CLIENT_SECRET,
+                'redirect_uri'  => self::REDIRECT_URI,
                 'grant_type'    => 'authorization_code'
             ]
         ]);
@@ -57,10 +64,15 @@ class SimklService {
             throw new \Exception('Token not found');
         }
 
+//        if ($this->cache->contains($code)) {
+//            return $this->cache->fetch($code);
+//        }
+//        $this->cache->save($code, $token);
+
         $response = $client->request('POST', '/sync/all-items/movies/?extended=full', [
             'headers' => [
                 'authorization' => 'Bearer ' . $token,
-                'simkl-api-key' => '9e36b457bc701379ef59849daf7ecf71e02549f551442b962b52a34b105c5d82'
+                'simkl-api-key' => self::CLIENT_ID
             ]
         ]);
 
